dcaf32771f0e52ac85360e8ff7a34965ab7a344f
Carlynn <chick04sd@gmail.com>
2017-08-23 11:21:51 -0700

    basic html and css created

---
+++ app.js
@@ -0,0 +1,23 @@
+console.log("linked");
+// wait for the DOM to finish loading
+$(document).ready(function() {
+
+
+})
+
+//Set up html
+  //Create background
+  //Place 3 images and give them classes and unique classes
+
+//Set up jquery
+//Need a start buton to allow users to begin the game
+//Create click functions for cartman and butters
+  //Click function will use a keypress and move the characters right one "square" at a time
+//Once start button is pressed kenny moves on his own to the right of the screen
+    //Move kenny at random speeds?
+//Define the endpoint
+//Whichever character, cartman or butters, reaches the end of the screen, that is the winner
+//Show an image of who Kenny dead
+//Show an image of who the winner is
+  //Transform this image in?
+//Create a reset button
